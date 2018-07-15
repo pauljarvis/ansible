022453c6ea577cf5fa9b83d5dd58b329df6ce705
Steve Stevens <steve.h.i.stevens@gmail.com>
2018-07-15 01:08:22 +0100

    ubuntu 1804 prereq

--- roles/scripts/add_launcher.sh
+++ roles/scripts/add_launcher.sh
@@ -1,3 +1,1 @@
-application="\'${1}.desktop\'"
-gsettings set com.canonical.Unity.Launcher favorites "$(gsettings get com.canonical.Unity.Launcher favorites | sed "s/, *${application} *//g" | sed "s/${application} *, *//g" | sed -e "s/]$/, ${application}]/")"
-
+add_unity_launcher.sh
