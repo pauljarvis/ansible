271d47e8261ec51fe51894d531608a1d8e6f6984
Steve Stevens <steve.h.i.stevens@gmail.com>
2018-10-06 03:13:28 +0100

    ansible-sys files are no longer seen by git as moving about

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,6 +6,7 @@ path_config="$(pwd)/.ansible-sys/ansible.cfg"
 path_hosts="$(pwd)/.ansible-sys/hosts"
 path_roles="$(pwd)/roles/"
 
+cp ${path_config}.origin ${path_config}
 sed -i 's:\(inventory.*=\).*:\1 '"${path_hosts}"':g' ${path_config}
 sed -i 's:\(roles_path.*=\).*:\1 '"${path_roles}"':g' ${path_config}
 
