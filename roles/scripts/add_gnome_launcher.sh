f7739aaacaf35436a62c023b077600b2d162d840
Steve Stevens <steven.stevens@travisperkins.co.uk>
2018-10-09 02:02:45 +0100

    launcher reform

--- roles/scripts/add_gnome_launcher.sh
+++ roles/scripts/add_gnome_launcher.sh
@@ -1,8 +1,12 @@
-application="'${1}.desktop'"
-favourites="/org/gnome/shell/favorite-apps"
-dconf write ${favourites} \
-  "$(dconf read ${favourites} \
-    | sed "s/, ${application}//g" \
-    | sed "s/${application}//g" \
-    | sed -e "s/]$/, ${application}]/")"
+application="${1}.desktop"
+favourites_source="/org/gnome/shell/favorite-apps"
+
+favourites_current="$(dconf read ${favourites_source})"
+found=$(echo $favourites_current | yq -r '.[] | select(.=="'${application}'")' )
+
+if [ -z "${found}" ] ; then
+  dconf write ${favourites_source} \
+    "$( dconf read ${favourites_source}  \
+      | sed -e "s/]$/, '${application}']/" )"
+fi 
 
--- roles/scripts/add_unity_launcher.sh
+++ roles/scripts/add_unity_launcher.sh
@@ -1,8 +1,12 @@
-application="'${1}.desktop'"
-favourites="/com/canonical/unity/launcher/favorites"
-dconf write ${favourites} \
-  "$(dconf read ${favourites} \
-    | sed "s/, ${application}//g" \
-    | sed "s/${application}//g" \
-    | sed -e "s/]$/, ${application}]/")"
+application="${1}.desktop"
+favourites_source="/com/canonical/unity/launcher/favorites"
+
+favourites_current="$(dconf read ${favourites_source})"
+found=$(echo $favourites_current | yq -r '.[] | select(.=="'${application}'")' )
+
+if [ -z "${found}" ] ; then
+  dconf write ${favourites_source} \
+    "$( dconf read ${favourites_source}  \
+      | sed -e "s/]$/, '${application}']/" )"
+fi 
 
