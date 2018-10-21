48a22ba3d16f193831a9d7f5e182adb4e257a83b
Steve Stevens <steve.h.i.stevens@gmail.com>
2018-10-21 17:14:02 +0100

    launcher refactored some more

--- roles/scripts/add_gnome_launcher.sh
+++ roles/scripts/add_gnome_launcher.sh
@@ -1,12 +1,27 @@
+#!/bin/bash
+
 application="${1}.desktop"
 favourites_source="/org/gnome/shell/favorite-apps"
 
-favourites_current="$(dconf read ${favourites_source})"
-found=$(echo $favourites_current | yq -r '.[] | select(.=="'${application}'")' )
+c_init() {
+  if [[ -z "$(c_read)" ]]
+    then $(c_write "['']"); fi
+}
+c_read() {
+  echo "$( dconf read "${favourites_source}" )"
+}
+c_write() {
+  echo "$( dconf write "${favourites_source}" "${1}" )"
+}
+already_exists() {
+  if [[ "$(c_read)" =~ .*${application}.* ]]
+    then echo true; else echo false; fi
+}
 
-if [ -z "${found}" ] ; then
-  dconf write ${favourites_source} \
-    "$( dconf read ${favourites_source}  \
-      | sed -e "s/]$/, '${application}']/" )"
+$(c_init)
+favourites_current="$(c_read)"
+if ! $(already_exists) ; then
+  favourites_new="$( echo $(c_read) | sed -e "s/]$/, '${application}']/" )"
+  $(c_write "${favourites_new}")
 fi
 
